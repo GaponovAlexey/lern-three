bc6bb575b50e053a1796f6662cf52c9919112d2b
Kekelberi <alexei.gaponov@gmail.com>
2022-03-17 19:20:15 +0300

    save

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  react: {
+    useSuspense: false,
+    wait: true,
+  },
+  experimental: {
+    concurrentFeatures: true,
+  },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,63 @@
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import type { NextPage } from 'next'
-import { useRef, useState } from 'react'
+import { Suspense, useRef, useState } from 'react'
+// import * as THREE from "three";
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { use } from '@react-three/cannon'
 
 const Home: NextPage = () => {
-  console.log('render')
-
   return (
     <div className='h-screen'>
-      <Canvas>
-        {/* свет */}
-        <ambientLight /> 
-        <pointLight position={[10, 1, 10]} />
-        <Box position={[-5, 0, -20]} />
-        <Box position={[0, 0, -20]} />
-        <Box position={[5, 0, -20]} />
-      </Canvas>
+      <Suspense fallback={null}>
+        <Canvas>
+          {/* свет  */}
+          <ambientLight intensity={0.1} />
+          {/* направленый свет */}
+          <pointLight position={[10, 1, 10]} />
+          <Box position={[-5, 0, -20]} />
+          <Box position={[0, 0, -20]} />
+          <Box position={[5, 0, -20]} />
+          <Floor position={[0, -4, -3]} />
+        </Canvas>
+      </Suspense>
     </div>
   )
 }
 
-function Box({...props}) {
+function Box(props) {
+  // const [create] = useLoader(TextureLoader, ["../public/img/1.jpg"])
+  const [create, two, three] = useLoader(TextureLoader, [
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/5.jpg',
+  ])
+
   const ref = useRef()
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  useFrame(()  => (ref.current.rotation.x += 0.01))
+  // useFrame(() => (ref.current.rotation.x += 0.01))
+
   return (
     <mesh
       {...props}
-      ref={ref}
+      // ref={ref}
       scale={clicked ? 5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
     >
       <boxGeometry args={[4, 4, 4]} />
-      <meshStandardMaterial color={hovered ? 'red' : 'coral'} />
+      <meshStandardMaterial map={hovered ? create : two} />
+    </mesh>
+  )
+}
+
+const Floor = (props) => {
+  const [create] = useLoader(TextureLoader, ['img/1.jpg'])
+  return (
+    <mesh {...props}>
+      <planeGeometry args={[12, 1]} />
+      <meshStandardMaterial map={create} />
     </mesh>
   )
 }
