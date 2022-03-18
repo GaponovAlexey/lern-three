6648b835c644744b0b83718c4871fe9dfa2a091c
Kekelberi <alexei.gaponov@gmail.com>
2022-03-18 11:46:56 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import CanMain from './CanMain'
+
+import CanMain from '../components/Canvas/CanMain'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <script src='./LearnCanvas.js' ></script>
-      </Head>
+      
       {/* <div className='fixed top-[50%] left-[40%]'>hi</div> */}
       {/* <One /> */}
       <CanMain />
